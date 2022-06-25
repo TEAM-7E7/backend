1bd35fd7e8adc9615487e32f5fb2778f6ce94770
imsunyong <imsunyong@naver.com>
2022-06-25 13:14:57 +0900

    첫 커밋

---
+++ src/main/java/com/example/selldio/SelldioApplication.java
@@ -0,0 +1,13 @@
+package com.example.selldio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelldioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelldioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/selldio/SelldioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selldio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelldioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
